8d7df6e8af4d7ba45cbcf9f43c3d613a95e0063b
tarikyesilcimenn <tarikyesilcimenn@gmail.com>
2022-10-20 12:18:30 +0300

    blog app
    blog app delete method is  added

--- src/views/ArticleDetail.vue
+++ src/views/ArticleDetail.vue
@@ -3,9 +3,13 @@
     <h1>Article Detail</h1>
     <div v-if="errors">{{errors}}</div>
     <div v-if="article" class="detail content">
-        <h2><i>Article Header : </i>{{article.header}}</h2>
-        <p><i>Article Content: </i>{{article.content}}</p>
-        <p><i>Created Date: </i>{{article.createdDate}}</p>
+        <div>
+            <h2><i>Article Header : </i>{{article.header}}</h2>
+            <p><i>Article Content: </i>{{article.content}}</p>
+            <p><i>Created Date: </i>{{article.createdDate}}</p>
+        </div>
+
+        <img width="45" height="45" class="delete" src="../../public/trashCan.png" alt="delete-article" @click="deleteArticle"/>
     </div>
     <div v-else>
         Loading...
@@ -14,19 +18,26 @@
 </template>
 <script>
 import { ref } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import getArticle from '../composables/getArticle.js'
+import { db } from '@/firebase/config';
 export default {
     setup() {
         const route = useRoute();
         const id = ref(route.params.id);
-
         const {article, errors, loadArticle} = getArticle(route.params.id);
+        const router = useRouter();
         loadArticle();
+        const deleteArticle  = async () => {
+            await db.collection('articles').doc(route.params.id).delete().then(() => {
+                router.push('/')
+            })
+        }
         return{
             id,
             article,
-            errors
+            errors,
+            deleteArticle
         }
     }
 }
@@ -43,8 +54,8 @@ export default {
         position: absolute;
         top: 0;
         right: 0;
-        border-radius: 50%;
-        padding: 8px;
+        padding: 0;
+        margin: 0;
         cursor: pointer;
     }
 </style>
